5d79e7525e45066b0505ae83ea4974af4c5a73c9
sheryl56 <shsepra@live.com>
2021-08-23 08:41:47 +1000

    Clean up based on Lint results

--- _tests_/user.test.js
+++ _tests_/user.test.js
@@ -1,4 +1,6 @@
 const AWSMock = require('aws-sdk-mock');
+const AWS = require('aws-sdk');
+AWS.config.update({region:'ap-southeast-2'});
 const util = require('util');
 const user = require('../src/api/user.js');
 const eventStub = require('./stubs/eventApiGatewayCreateUser.json');
@@ -27,25 +29,25 @@ describe('User service test: Dynamodb mock for successful operations', () => {
     delete process.env.KMS_PASSWORD_ENCRYPT_KEY;
   });
 
-  test('Replies back with a JSON for get', () => {
+  test('Replies back with a JSON for get', async() => {
     process.env.USER_TABLE = 'foo';
     process.env.KMS_PASSWORD_ENCRYPT_KEY = 'bar';
 
     const event = eventStub;
     const context = {};
 
-    const result = handlerList(event, context);
+    const result = await handlerList(event, context);
     expect(result).resolves.toMatchSnapshot();
   });
 
-  test('Replies back with success message for put', () => {
+  test('Replies back with success message for put', async() => {
     process.env.USER_TABLE = 'foo';
     process.env.KMS_PASSWORD_ENCRYPT_KEY = 'bar';
 
     const event = eventStub;
     const context = {};
 
-    const result = handlerSubmit(event, context);
+    const result = await handlerSubmit(event, context);
     expect(result).resolves.toMatchSnapshot();
   });
 });
